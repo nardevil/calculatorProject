af5079c48bd98f394783ac7cb7264a0015e931bb
vprica3@gmail.com <vprica3@gmail.com>
2018-10-19 01:00:43 +0200

    twelfth commit

--- src/main/java/com/vp/calculator/CalculatorApplication.java
+++ src/main/java/com/vp/calculator/CalculatorApplication.java
@@ -2,6 +2,11 @@ package com.vp.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @SpringBootApplication
 public class CalculatorApplication {
@@ -9,4 +14,15 @@ public class CalculatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/calculate").allowedOrigins("https://mb-calculator-react.firebaseapp.com/");
+            }
+        };
+    }
+
 }
