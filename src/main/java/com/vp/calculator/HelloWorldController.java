b46277db4af40e522235e51bc33c8348344cb85a
vprica3@gmail.com <vprica3@gmail.com>
2018-10-18 08:44:01 +0200

    second commit

--- src/main/java/com/vp/calculator/HelloWorldController.java
+++ src/main/java/com/vp/calculator/HelloWorldController.java
@@ -3,13 +3,41 @@ package com.vp.calculator;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.json.*;
+import com.fathzer.soft.javaluator.DoubleEvaluator;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping("/hello")
-    public String helloWorldMethod(@RequestParam(value = "broj1") String broj1, @RequestParam(value = "broj2") String broj2){
-        double result = Double.valueOf(broj1) + Double.valueOf(broj2);
-        return String.valueOf(result);
+    /**@CrossOrigin(origins = "http://localhost:3000")*/
+    @RequestMapping("/calculate")
+    public String helloWorldMethod(@RequestParam(value = "operations") String operations){
+
+        boolean isValid = true;
+        JSONObject response = new JSONObject();
+        response.put("message", "error");
+        response.put("operations", "Error - wrong input");
+
+        if(!Character.isDigit(operations.charAt(0)))
+        {
+            return response.toString();
+        }
+        else{
+            for (int i = 0; i < operations.length()-1; i++){
+                if(Character.isDigit(operations.charAt(i)) || Character.isDigit(operations.charAt(i+1)))
+                {
+                }
+                else {
+                    return response.toString();
+                }
+                }
+            }
+
+        Double operationResult = new DoubleEvaluator().evaluate(operations);
+        response.put("message", "success");
+        response.put("operations", String.valueOf(operationResult));
+        return response.toString();
+        }
     }
-}
+
